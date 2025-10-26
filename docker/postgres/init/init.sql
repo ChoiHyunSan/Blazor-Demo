7c98ab4c0763ea0e6edb65d85a7ad7e92cedf465
ChoiHyunSan <chsan626@gmail.com>
2025-10-26 21:53:57 +0900

    [DB] note_adv 테이블 추가

--- docker/postgres/init/init.sql
+++ docker/postgres/init/init.sql
@@ -3,3 +3,12 @@ create table if not exists notes (
   title text not null,
   created_at timestamptz not null default now()
 );
+
+create table if not exists notes_adv (
+  id          bigserial primary key,
+  title       text        not null,
+  content     text        null,
+  created_at  timestamptz not null default now(),
+  updated_at  timestamptz not null default now(),
+  archived    boolean     not null default false
+);
