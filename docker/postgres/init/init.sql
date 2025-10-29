04429c258bbde0fe50b6dfadee2d8621281428ab
ChoiHyunSan <chsan626@gmail.com>
2025-10-30 00:55:15 +0900

    [DB] User & Mail 테이블 스키마 추가

--- docker/postgres/init/init.sql
+++ docker/postgres/init/init.sql
@@ -5,10 +5,24 @@ create table if not exists notes (
 );
 
 create table if not exists notes_adv (
-  id          bigserial primary key,
+  id          bigserial   primary key,
   title       text        not null,
   content     text        null,
   created_at  timestamptz not null default now(),
   updated_at  timestamptz not null default now(),
   archived    boolean     not null default false
+);
+
+create table if not exists users (
+	id 		serial 	primary key,
+    name 	text 	not null
+);
+
+create table if not exists mails (
+	id 			serial 		primary key,
+    user_id 	int 		not null,
+    title  		text		not null,
+    body 		text 		not null,
+    item_id 	integer 	not null,
+    item_count	integer 	not null,
 );
