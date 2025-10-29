71401a931b5c7114282c601730c5dd75e5ca34a6
ChoiHyunSan <chsan626@gmail.com>
2025-10-30 01:30:01 +0900

    [FEAT] User Mail 도메인 클래스 추가

---
+++ BlazorDashboard/Models/UserMail.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorDashboard.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class Mail
+    {
+        public int Id { get; set; }
+        public string title { get; set; } = string.Empty;
+        public string body { get; set; } = string.Empty;
+        public int itemId { get; set; }
+        public int itemCount { get; set; }
+    }
+}
