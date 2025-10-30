0cd0ae19bfa520a0a302881c833d7184d72a82f7
ChoiHyunSan <chsan626@gmail.com>
2025-10-30 18:53:28 +0900

    [FEAT] User 목록 조회 & User 선택 서비스 로직 및 화면 구현

--- BlazorDashboard/Services/UserMailService.cs
+++ BlazorDashboard/Services/UserMailService.cs
@@ -1,6 +1,34 @@
-﻿namespace BlazorDashboard.Services
+﻿using BlazorDashboard.Models;
+using Dapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Npgsql;
+
+namespace BlazorDashboard.Services
 {
     public class UserMailService
     {
+        private readonly NpgsqlDataSource _dataSource;
+
+        public UserMailService(NpgsqlDataSource dataSource)
+        {
+            _dataSource = dataSource;
+        }
+
+        public async Task<List<User>> GetUsersAsync()
+        {
+            const string sql = """
+                SELECT Id, Name
+                FROM users
+                ORDER BY Id ASC
+            """;
+
+            await using var conn = await _dataSource.OpenConnectionAsync();
+            var result = await conn.QueryAsync<User>(sql);
+
+            return result.AsList();
+        } 
+    
+
     }
 }
