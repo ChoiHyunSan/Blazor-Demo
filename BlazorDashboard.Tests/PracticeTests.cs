73d6d1aaee581a3c66f9bfecc0b9a03b23dc7c28
ChoiHyunSan <chsan626@gmail.com>
2025-11-03 23:43:40 +0900

    BUnit Test Code 추가

---
+++ BlazorDashboard.Tests/PracticeTests.cs
@@ -0,0 +1,29 @@
+﻿using Bunit;
+using System;
+using Xunit;
+
+public class Class1 : TestContext
+{
+    public class UserService
+    {
+        public bool IsAdult(int age) => age >= 19;
+    }
+
+	[Fact]
+	public void Method()
+	{
+	    var result = _sut.IsAdult(20);
+        Assert.True(result);
+    }
+
+    private readonly UserService _sut = new();
+
+    [Theory]
+    [InlineData(19, true)]
+    [InlineData(18, false)]
+    public void IsAdult_ReturnsExpected(int age, bool expected)
+    {
+        var ok = _sut.IsAdult(age);
+        Assert.Equal(expected, ok);
+    }
+}
